bce7f192e7f53d46869b69c433d42d2eaab6abe4
David DeSandro <desandrocodes@gmail.com>
2014-03-02 10:24:04 -0500

    start with gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*jshint node: true, strict: false */
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task( 'scripts', function() {
+  gulp.src([
+      'js/vector.js',
+      'js/particle.js',
+      'js/breathing-halftone.js'
+    ])
+    .pipe( concat('breathing-halftone.pkgd.js') )
+    .pipe( gulp.dest('./dist/') );
+});
