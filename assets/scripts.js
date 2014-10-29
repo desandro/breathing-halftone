f6f34494a0d41723b5d3b6d8c374ed889db76620
David DeSandro <desandrocodes@gmail.com>
2014-10-29 18:00:20 -0400

    add other images for other demos

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,9 +13,34 @@ function init() {
   }
   isInited = true;
 
-  var img = document.querySelector('#hero img');
-  new BreathingHalftone( img );
+  // var img = document.querySelector('.sarah');
+  // var halftone = new BreathingHalftone( img );
 
+  // var img = document.querySelector('.ncsu');
+  // var halftone = new BreathingHalftone( img, {
+  //   isAdditive: true,
+  //   isRadial: true,
+  //   friction: 0.04,
+  //   displacement: {
+  //     hoverDiameter: 0.8,
+  //     hoverForce: 0.007,
+  //     activeDiameter: 0.8,
+  //     activeForce: -0.007
+  //   }
+  // });
+
+  var img = document.querySelector('.the-look');
+  var halftone = new BreathingHalftone( img, {
+    dotSize: {
+      diameter: 1/70,
+      initVelocity: 0.05,
+      oscAmplitude: 0
+    },
+    friction: 0.05,
+    channels: [ 'lum' ]
+  });
+
+  window.halftone = halftone;
 }
 
 document.addEventListener( 'DOMContentLoaded', init, false );
