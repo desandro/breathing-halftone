1e5c04d527e18fb12d580228a53936a106616a50
David DeSandro <desandrocodes@gmail.com>
2014-10-30 21:52:14 -0400

    set multiple examples

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,31 +6,23 @@ var BreathingHalftone = window.BreathingHalftone;
 
 var isInited = false;
 
-function init() {
-  // init once
-  if ( isInited ) {
-    return;
-  }
-  isInited = true;
+var halftone;
 
-  // var img = document.querySelector('.sarah');
-  // var halftone = new BreathingHalftone( img );
-
-  // var img = document.querySelector('.ncsu');
-  // var halftone = new BreathingHalftone( img, {
-  //   isAdditive: true,
-  //   isRadial: true,
-  //   friction: 0.04,
-  //   displacement: {
-  //     hoverDiameter: 0.8,
-  //     hoverForce: 0.007,
-  //     activeDiameter: 0.8,
-  //     activeForce: -0.007
-  //   }
-  // });
-
-  var img = document.querySelector('.the-look');
-  var halftone = new BreathingHalftone( img, {
+// options for each demo
+var demoOptions = {
+  sarah: {},
+  ncsu: {
+    isAdditive: true,
+    isRadial: true,
+    friction: 0.04,
+    displacement: {
+      hoverDiameter: 0.8,
+      hoverForce: 0.007,
+      activeDiameter: 0.8,
+      activeForce: -0.007
+    }
+  },
+  'the-look': {
     dotSize: {
       diameter: 1/70,
       initVelocity: 0.05,
@@ -38,9 +30,48 @@ function init() {
     },
     friction: 0.05,
     channels: [ 'lum' ]
-  });
+  }
+};
 
-  window.halftone = halftone;
+function init() {
+  // init once
+  if ( isInited ) {
+    return;
+  }
+  isInited = true;
+
+  var thumbnailRail = document.querySelector('.thumbnails');
+  var setName;
+
+  function initHalftone( name ) {
+    // do not re-init
+    if ( name === setName ) {
+      return;
+    }
+    var img = document.querySelector( 'img.' + name );
+    var opts = demoOptions[ name ];
+    if ( halftone ) {
+      halftone.destroy();
+    }
+    halftone = new BreathingHalftone( img, opts );
+    window.halftone = halftone;
+    setName = name;
+  }
+
+  initHalftone('sarah');
+
+  thumbnailRail.addEventListener( 'click', onThumbnailClick, false );
+
+  function onThumbnailClick( event ) {
+    if ( event.target.nodeName !== 'IMG' ) {
+      return;
+    }
+    var name = event.target.getAttribute('data-name');
+    initHalftone( name );
+    thumbnailRail.querySelector('.is-selected').className = '';
+    event.target.className = 'is-selected';
+  }
+
 }
 
 document.addEventListener( 'DOMContentLoaded', init, false );
